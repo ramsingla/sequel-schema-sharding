9307dfac2751e7b7f2e4bedf1af9344ddbd6277f
Ram Singla <ram.singla@gmail.com>
2014-05-07 18:55:02 +0530

    Update 001_create_artists.rb
    fixing migration

--- examples/db/migrations/artists/001_create_artists.rb
+++ examples/db/migrations/artists/001_create_artists.rb
@@ -1,14 +1,10 @@
 Sequel.migration do
-  up do
-    create_table :artists do
+  change do
+    create_table migration_schema_for_table(:artists) do
       Integer :id, null: false
       String :name, null: false
       Time :created_at
     end
-  end
-
-  down do
-    drop_table :artists
   end
 end
 
